93a70f46fb6d23400f9ff756b621baf900638468
Jeru Balares <jerupaulbalares@gmail.com>
2022-09-25 23:39:22 -0700

    Added an if-else statement that saves the number displayed into a variable and clears the display when an operator button is press (such as +, -, etc.). Added an else statement that saves the displayed number into a variable which then inputs both the previous and current variables into the operator object, inputting the result into the display.

--- scripts.js
+++ scripts.js
@@ -1,33 +1,34 @@
 var operation = {
     multiply: function (a, b) {
-        return a * b;
+        return (+a * +b);
     },
     divide: function (a, b) {
         if (a !== 0 && b === 0) {
             return 'You tryna break me?';
-        } else return a / b;
+        } else return (+a / +b);
     },
     add: function (a, b) {
-        return a + b;
+        return (+a + +b);
     },
     minus: function (a, b) {
-        return a - b;
+        return (+a - +b);
     },
-    calculate: false,
-    equals: function () {
-        this.calculate = false;
-    }
 };
 
-function operatorr(a, b, operate) {
-    var c = operation.operate(a, b);
-    return c;
-};
-
-
+let a = 0;
+let operator = '';
 const container = document.querySelector(".button-container");
 container.addEventListener("click", function (e) {
     const display = document.getElementById("display");
-    console.log(e.target.value);
-    display.appendChild(document.createTextNode(e.target.value));
+    if (e.target.value) {
+        display.appendChild(document.createTextNode(e.target.value));
+    } else if (e.target.id !== 'equals') {
+        a = display.innerHTML;
+        operator = e.target.id;
+        display.innerHTML = '';
+    } else {
+        const b = display.innerHTML;
+        display.innerHTML = '';
+        display.innerHTML = operation[`${operator}`](a, b);
+    }
 });
